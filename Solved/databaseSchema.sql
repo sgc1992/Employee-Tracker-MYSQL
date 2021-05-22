5f144120a6faa2bdb121ff266cf7bd6c5c9ea40c
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-22 15:16:32 +1000

    added table for employee

--- Solved/databaseSchema.sql
+++ Solved/databaseSchema.sql
@@ -14,3 +14,10 @@ CREATE TABLE role(
   salary DECIMAL (10,2) NULL,
   department_id INT NULL
 );
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
