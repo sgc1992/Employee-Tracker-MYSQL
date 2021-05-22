1b0e925a74b6e52efcb42abc87c563e82afd8f4a
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-23 09:16:39 +1000

    added js for the connection to the data base

--- Schema/seed.sql
+++ Schema/seed.sql
@@ -8,26 +8,25 @@ VALUES
 ('Engineering'),
 ('Finance');
 
----- Role Seed ----
+---- Titlerole Seed ----
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO titleRole (title, salary, department_id)
 VALUES 
 ('Managing Director', 150000, 1),
 ('Head of Technology', 120000, 2),
 ('Head of Finace', 100000, 3),
 ('Customer Care Manager', 90000, 4),
-('Human Resource Officer', 85000, 5),
-
+('Human Resource Officer', 85000, 5);
 
 ---- Employee Seed ----
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Sandeep', 'GC', 1, null),
-('Sofiya', 'Rayamajhi', 2, 1),
-('Susma', 'Chhetri', 2, 1),
-('Mekh', 'GC', 4, 1),
-('SanuNani', 'Khatri', 3, 1),
+('Sandeep', 'GC', 1, 001),
+('Sofiya', 'Rayamajhi', 2, 002),
+('Susma', 'Chhetri', 2, 003),
+('Mekh', 'GC', 4, 004),
+('SanuNani', 'Khatri', 3, 005);
 
 
 
